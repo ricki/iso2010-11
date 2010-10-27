a7e28bb37234de0612620d1e74cbc2b94b768ef8
Antonio Gomez <pobleteag@gmail.com>
2010-10-27 10:08:34 +0200

    Se ha eliminado la clase RegisterWuindow y ahora el RegisterDialog hereda de JDialog.

--- src/com/umbrella/worldconq/ui/RegisterWindow.java
+++ src/com/umbrella/worldconq/ui/RegisterWindow.java
@@ -1,15 +1,9 @@
 package com.umbrella.worldconq.ui;
 
-import java.awt.Dialog;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class RegisterWindow extends JFrame {
-	private RegisterDialog d;
-}
-
-public class RegisterDialog extends Dialog{
+public class RegisterDialog extends JDialog {
 	
 	private JPanel registerPanel;
 }
