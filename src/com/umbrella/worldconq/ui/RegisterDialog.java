5ecdf172f17ff9bbcb01924c5c93cf0992626013
antonio <antonio@antonio-desktop.(none)>
2010-10-30 13:59:51 +0200

    Añadiendo artibutos a RegiterDialog

--- src/com/umbrella/worldconq/ui/RegisterDialog.java
+++ src/com/umbrella/worldconq/ui/RegisterDialog.java
@@ -10,19 +10,51 @@ public class RegisterDialog extends JDialog {
 	
 	private JPanel registerPanel;
 	private JButton CancelButton, AcceptButton;
-	private boolean selection;
+	
 
+	private JLabel mapLabel;
+	
+	private JLabel UserLabel;
+	private JLabel EmailLabel;
+	private JLabel PasswdLabel;
+	
+	private JTextField UserTextField;
+	private JTextField EmailTextField;
+	private  JPasswordField PasswdField;
+	
+	private boolean selection;
+	
 	public RegisterDialog(JFrame f, String string, boolean b) {
 		super(f, string, b);
-		JPanel p = new JPanel();
-		AcceptButton = new JButton("Aceptar");
-		CancelButton = new JButton("Cancelar");
-		AcceptButton.addMouseListener(new AcceptDialogAdapter(this, true));
-		CancelButton.addMouseListener(new AcceptDialogAdapter(this, false));
-		p.add(AcceptButton);
-		p.add(CancelButton);
-		this.setContentPane(p);
-		this.pack();
+		initGUI(f);
+	}
+	
+		private void initGUI(JFrame f) {
+		try {
+			JPanel p = new JPanel();
+			try {
+				this.setIconImage(new ImageIcon(getClass().getClassLoader().getResource("home/antonio/workspace/iso2010-11/src/image/umbrella.png")).getImage());
+			} catch (Exception e) {
+				System.out.println("Imagen no encontrada");
+			}
+			
+			AcceptButton = new JButton("Aceptar");
+			CancelButton = new JButton("Cancelar");
+			AcceptButton.addMouseListener(new AcceptDialogAdapter(this, true));
+			CancelButton.addMouseListener(new AcceptDialogAdapter(this, false));
+			p.add(AcceptButton);
+			p.add(CancelButton);
+			
+			map = new JLabel();
+			//map.setIcon(new ImageIcon(getClass().getClassLoader().getResource("src/images/mapa.png")));
+			map.setBounds(0, 6, 472, 257);
+			p.add(map);
+						
+			this.setContentPane(p);
+			this.pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean getSelection() {
