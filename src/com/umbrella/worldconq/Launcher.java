58f4f3a8925e114ea240d41b565cb1a71a820c6d
Ricardo Ruedas <ricardo.ruedas@gmail.com>
2010-12-05 20:21:20 +0100

    Nuevas funciones para usar el 'ServerAdapter'.

--- src/com/umbrella/worldconq/Launcher.java
+++ src/com/umbrella/worldconq/Launcher.java
@@ -8,13 +8,13 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		{
-			try {
-				javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
-			} catch(Exception e) {
-				e.printStackTrace();
-			}
+		try {
+			javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch(Exception e) {
+			e.printStackTrace();
 		}
+		
+		System.setProperty("java.security.policy", ClassLoader.getSystemResource("data/open.policy").toString());
 		
 		StartupWindow stw = new StartupWindow();
 		stw.setLocationRelativeTo(null);
--- src/com/umbrella/worldconq/comm/ServerAdapter.java
+++ src/com/umbrella/worldconq/comm/ServerAdapter.java
@@ -1,13 +1,32 @@
 package com.umbrella.worldconq.comm;
 
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.util.UUID;
+
+import com.umbrella.worldconq.stubserver.IServer;
 
 public class ServerAdapter {
 
 	protected static ServerAdapter mInstance = null;
 	
-	protected ServerAdapter(){
-		System.setProperty("java.security.policy", ClassLoader.getSystemResource("data/open.policy").toString());
+	private String mRemoteName;
+	private InetAddress mRemoteHost;
+	private int mRemotePort;
+	private IServer mProxy;
+	
+	protected ServerAdapter() {
+		mRemoteName = null;
+		mRemoteHost = null;
+		mRemotePort = 0;
+		mProxy = null;
+	}
+	
+	protected ServerAdapter(String remoteName, InetAddress remoteHost, int remotePort) {
+		setRemoteInfo(remoteName, remoteHost, remotePort);
 	}
 	
 	public static ServerAdapter getServerAdapter(){
@@ -16,6 +35,30 @@ public class ServerAdapter {
 		}
 		
 		return mInstance;
+	}
+	
+	public void setRemoteInfo(String remoteName, InetAddress remoteHost, int remotePort) {
+		mRemoteName = remoteName;
+		mRemoteHost = remoteHost;
+		mRemotePort = remotePort;
+	}
+	
+	public void connect() throws MalformedURLException, RemoteException, NotBoundException {
+		disconnect();
+		String url = String.format("rmi://%s:%d/%s", 
+				mRemoteHost.getHostAddress(),
+				mRemotePort,
+				mRemoteName
+		);
+		mProxy = (IServer) Naming.lookup(url);
+	}
+	
+	public void disconnect() {
+		mProxy = null;
+	}
+	
+	public boolean isConnected() {
+		return mProxy != null;
 	}
 	
 	public UUID validateUser(String Login, String Passwd) {
