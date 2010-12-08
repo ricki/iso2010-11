99898aa8d3d5fdd93c8f44142c6a27e9e6c95bca
Ricardo Ruedas <ricardo.ruedas@gmail.com>
2010-12-09 00:27:28 +0100

    Añadidas las dos ventanas (StartupWindow y MainWindow) a la clase WorldConqApp.

--- src/com/umbrella/worldconq/Launcher.java
+++ src/com/umbrella/worldconq/Launcher.java
@@ -2,36 +2,29 @@ package com.umbrella.worldconq;
 
 import java.net.InetAddress;
 
-import com.umbrella.worldconq.comm.ServerAdapter;
-import com.umbrella.worldconq.ui.StartupWindow;
-
 public class Launcher {
 
 	/**
 	 * @param args
+	 * @throws Exception
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		try {
 			javax.swing.UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		System.setProperty("java.security.policy", ClassLoader.getSystemResource("data/open.policy").toString());
-		
-		try {
-			WorldConqApp.getServerAdapter().setRemoteInfo(
-				"WorldConqStubServer",
-				InetAddress.getByName("localhost"),
-				3234
-			);
-			WorldConqApp.getServerAdapter().connect();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		StartupWindow stw = new StartupWindow();
-		stw.setLocationRelativeTo(null);
-		stw.setVisible(true);
+
+		WorldConqApp.getServerAdapter().setRemoteInfo(
+			"WorldConqStubServer",
+			InetAddress.getByName("localhost"),
+			3234
+		);
+		WorldConqApp.getServerAdapter().connect();
+
+		WorldConqApp.getStartupWindow().setLocationRelativeTo(null);
+		WorldConqApp.getStartupWindow().setVisible(true);
 	}
 }	
