bde1c0eb6208f6b9709819182c38b441c1855a76
Antonio Gómez Poblete <pobleteag@gmail.com>
2010-12-18 23:48:29 +0100

    Añadido Clase UserManagerTest

---
+++ src/com/umbrella/worldconq/testing/UserManagerTest.java
@@ -0,0 +1,25 @@
+package com.umbrella.worldconq.testing;
+
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class UserManagerTest  extends TestCase {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	
+	
+	public void testRegisterUser() {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
