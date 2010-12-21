2cfb33d228bc5395ab616742567aa55bde92c92e
Daniel León Romero <daniel.leonromero@gmail.com>
2010-12-21 19:38:09 +0100

    Filtro de partidas

--- src/com/umbrella/worldconq/domain/UserManager.java
+++ src/com/umbrella/worldconq/domain/UserManager.java
@@ -1,14 +1,18 @@
 package com.umbrella.worldconq.domain;
 
+import java.util.UUID;
+
 import com.umbrella.worldconq.WorldConqApp;
 
 
 public class UserManager {
 	
 	private Session mSession;
+	private User mUser;
 	
 	public UserManager(){
 		 mSession = null;
+		 setmUser(null);
 	}
 	
 	public void registerUser(String Login, String Passwd, String Email) throws Exception {
@@ -18,6 +22,8 @@ public class UserManager {
 	public void createSession(String Login, String Passwd) throws Exception {
 		// TODO Comprobar si hay sesión activa y cerrarla antes.
 		mSession = WorldConqApp.getServerAdapter().createSession(Login, Passwd);
+		// TODO Poner el email y el uuid correcto
+		setmUser(new User(UUID.randomUUID(), Login, "asdf@asfd.adsf"));
 	}
 
 	public void closeSession() throws Exception {
@@ -27,5 +33,13 @@ public class UserManager {
 	
 	public Session getActiveSession() {
 		return mSession;
+	}
+
+	public void setmUser(User mUser) {
+		this.mUser = mUser;
+	}
+
+	public User getmUser() {
+		return mUser;
 	}
 }
