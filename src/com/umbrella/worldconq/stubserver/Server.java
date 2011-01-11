53a34c106734612680691e190be46249d3dee5d4
Jorge Colao Adan <jorge.colao@gmail.com>
2011-01-11 19:42:54 +0100

    Tabla en orden del server.java

--- src/com/umbrella/worldconq/stubserver/Server.java
+++ src/com/umbrella/worldconq/stubserver/Server.java
@@ -11,6 +11,7 @@ import java.util.Calendar;
 import java.util.UUID;
 
 import com.umbrella.worldconq.domain.Session;
+import com.umbrella.worldconq.domain.TerritoryData;
 
 import es.uclm.iso2.rmi.Arsenal;
 import es.uclm.iso2.rmi.EventType;
@@ -18,6 +19,7 @@ import es.uclm.iso2.rmi.Game;
 import es.uclm.iso2.rmi.GameInfo;
 import es.uclm.iso2.rmi.IServer;
 import es.uclm.iso2.rmi.Player;
+import es.uclm.iso2.rmi.Spy;
 import es.uclm.iso2.rmi.Territory;
 import es.uclm.iso2.rmi.Territory.Continent;
 import es.uclm.iso2.rmi.exceptions.GameNotFoundException;
@@ -86,6 +88,9 @@ public class Server extends UnicastRemoteObject implements IServer {
 			registerUsers.add(user);
 
 		{ // GameInfo 01
+			final int[] p = {
+					1, 2, 3
+			};
 			final Game testGame = new Game();
 			final ArrayList<String> player = new ArrayList<String>();
 			// new Player("JorgeCA", 1000, true, true, new ArrayList<Spy>());
@@ -100,21 +105,25 @@ public class Server extends UnicastRemoteObject implements IServer {
 			testGame.setGameInfo(gameList.get(0));
 			// lista de jugadores
 			final ArrayList<Player> playerList = new ArrayList<Player>();
-			playerList.add(new Player("JorgeCA", 200, true, true, null));
-			playerList.add(new Player("Aduran", 250, true, false, null));
+			final ArrayList<Spy> spyList = new ArrayList<Spy>();
+			final Territory t = new Territory(3, Territory.Continent.Europe,
+				null, 10, p, 2, 0, 1); // territorio Angel
+			playerList.add(new Player("Aduran", 250, true, false,
+				new ArrayList<Spy>()));
+			t.setOwner(playerList.get(0));
+			spyList.add(new Spy(2, t));
+			playerList.add(new Player("JorgeCA", 200, true, true, spyList));
 			testGame.setPlayers(playerList);
 			//añadimos los datos a la partida necesarios para la clase Territory
 			final ArrayList<Territory> mapList = new ArrayList<Territory>();
 			this.rellenarMapaInicial(mapList);
-			final int[] p = {
-					1, 2, 3
-			};
-			this.borrarCosasMapa(mapList, 1, Territory.Continent.Europe);
-			mapList.add(new Territory(1, Territory.Continent.Europe,
-				playerList.get(0), 20, p, 1, 0, 1));
-			this.borrarCosasMapa(mapList, 3, Territory.Continent.Europe);
-			mapList.add(new Territory(3, Territory.Continent.Europe,
-				playerList.get(1), 10, p, 2, 0, 1));
+
+			mapList.remove(TerritoryData.getIndex(Territory.Continent.Europe, 1));
+			mapList.add(TerritoryData.getIndex(Territory.Continent.Europe, 1),
+				new Territory(1, Territory.Continent.Europe,
+					playerList.get(1), 20, p, 1, 0, 1));
+			mapList.remove(TerritoryData.getIndex(t));
+			mapList.add(TerritoryData.getIndex(t), t);
 			testGame.setMap(mapList);
 			testGameList.add(testGame);
 		}
