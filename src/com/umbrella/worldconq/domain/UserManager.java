e309a62daf624c32dc1bc7a5cc18b1e299b3a4c0
Antonio Gómez Poblete <pobleteag@gmail.com>
2011-01-29 12:38:12 +0100

    Añadida InvalidArgumentException, filtramos capos erroneos en UserManager createSesion y register user y camputamos las excepcioones en la interfaza

--- src/com/umbrella/worldconq/domain/UserManager.java
+++ src/com/umbrella/worldconq/domain/UserManager.java
@@ -1,13 +1,17 @@
 package com.umbrella.worldconq.domain;
 
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.umbrella.worldconq.WorldConqApp;
+import com.umbrella.worldconq.exception.InvalidArgumentException;
 
 public class UserManager {
 
 	private final WorldConqApp app;
 	private Session mSession;
+	private final String emailReEx = "^[A-Za-z0-9_%-]+@[A-Za-z0-9_%-]+.[A-Za-z0-9_%-]{2,4}$";
 
 	public UserManager() {
 		app = WorldConqApp.getWorldConqApp();
@@ -18,15 +22,29 @@ public class UserManager {
 		return mSession;
 	}
 
-	public void registerUser(String login, String passwd, String email) throws Exception {
-		app.getServerAdapter().registerUser(login, passwd, email);
+	public void registerUser(String login, String passwd, String email) throws InvalidArgumentException, Exception {
+		if (login == null || passwd == null || email == null ||
+				login.equals("") || passwd.equals("") || email.equals("")) {
+			throw new InvalidArgumentException();
+		} else {
+			final Pattern p = Pattern.compile(emailReEx);
+			final Matcher m = p.matcher(email);
+			if (m.find())
+				app.getServerAdapter().registerUser(login, passwd, email);
+			else
+				throw new InvalidArgumentException();
+		}
 	}
 
 	public void createSession(String login, String passwd) throws Exception {
-		if (mSession != null) this.closeSession();
-		final UUID id = app.getServerAdapter().createSession(login, passwd);
-		mSession = new Session(id, login);
-		app.setMainMode();
+		if (login == null || passwd == null || login == "" || passwd == "")
+			throw new InvalidArgumentException();
+		else {
+			if (mSession != null) this.closeSession();
+			final UUID id = app.getServerAdapter().createSession(login, passwd);
+			mSession = new Session(id, login);
+			app.setMainMode();
+		}
 	}
 
 	public void closeSession() throws Exception {
---
+++ src/com/umbrella/worldconq/exception/InvalidArgumentException.java
@@ -0,0 +1,10 @@
+package com.umbrella.worldconq.exception;
+
+public class InvalidArgumentException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
