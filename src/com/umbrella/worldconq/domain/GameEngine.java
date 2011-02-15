ccc28f9c7753bdb5f58ef2059961b4748c2f0778
Antonio Gómez Poblete <pobleteag@gmail.com>
2011-02-16 00:24:10 +0100

    Todas las cabezaras de los metodos creados de GameEmgine

--- src/com/umbrella/worldconq/domain/GameEngine.java
+++ src/com/umbrella/worldconq/domain/GameEngine.java
@@ -1,10 +1,18 @@
 package com.umbrella.worldconq.domain;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 import com.umbrella.worldconq.comm.ServerAdapter;
+import communications.IClient.TimeType;
 
+import domain.Arsenal;
+import domain.EventType;
 import domain.Game;
+import domain.Player;
+import domain.Territory;
 
-public class GameEngine {
+public class GameEngine implements ClientCallback {
 	private MapModel mMapListModel;
 	private PlayerListModel mPlayerListModel;
 	private final Game mGame;
@@ -27,19 +35,86 @@ public class GameEngine {
 		mPlayerListModel.setData(mGame.getPlayers());
 	}
 
-	public void setMapListModel(MapModel mMapListModel) {
-		this.mMapListModel = mMapListModel;
+	public UUID getId() {
+		return mGame.getGameInfo().getId();
+	}
+
+	public String getName() {
+		return mGame.getGameInfo().getName();
 	}
 
 	public MapModel getMapListModel() {
 		return mMapListModel;
 	}
 
-	public void setPlayerListModel(PlayerListModel mPlayerListModel) {
-		this.mPlayerListModel = mPlayerListModel;
+	public String getDescription() {
+		return mGame.getGameInfo().getDescription();
 	}
 
 	public PlayerListModel getPlayerListModel() {
 		return mPlayerListModel;
+
+	}
+
+	public void setMapListModel(MapModel mMapListModel) {
+		this.mMapListModel = mMapListModel;
+	}
+
+	public void setPlayerListModel(PlayerListModel mPlayerListModel) {
+		this.mPlayerListModel = mPlayerListModel;
+	}
+
+	public void attackTerritory(int src, int dst, int soldiers, int cannons, int missiles, int icbm) throws Exception {
+
+	}
+
+	public void acceptAttack() {
+
+	}
+
+	public void requestNegotiation(int money, int soldiers) throws Exception {
+
+	}
+
+	public void buyUnits(int erritory, int soldiers, int cannons, int missiles, int ICMB, int antimissiles) throws Exception {
+
+	}
+
+	public void moveUnits(int src, int dst, int soldiers, int cannons, int missiles, int ICMB, int antimissiles) throws Exception {
+
+	}
+
+	public void buyTerritory(int territory) {
+
+	}
+
+	@Override
+	public void territoryUnderAttack(Territory src, Territory dst, Arsenal arsenal) {
+
+	}
+
+	@Override
+	public void negotiationRequested(int money, int soldiers) {
+
+	}
+
+	@Override
+	public void resolveAttack() {
+
+	}
+
+	@Override
+	public void resolveNegotiation(int money, int sodiers) {
+
+	}
+
+	@Override
+	public void updateClient(ArrayList<Player> playerUpdate, ArrayList<Territory> territoryUpdate, EventType event) {
+
+	}
+
+	@Override
+	public void timeExpired(TimeType whatTime) {
+
 	}
 }
