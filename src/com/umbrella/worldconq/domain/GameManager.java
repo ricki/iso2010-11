30113fc93ceac62e9075f793b826f631aa6f8403
Jorge Colao Adan <jorge.colao@gmail.com>
2011-02-19 21:53:51 +0100

    Filtro de la hora de las partidas y un fallo de mapView

--- src/com/umbrella/worldconq/domain/GameManager.java
+++ src/com/umbrella/worldconq/domain/GameManager.java
@@ -58,13 +58,33 @@ public class GameManager {
 		final ArrayList<GameInfo> fullList = srvAdapter.fetchGameList(usrMgr.getSession());
 		final ArrayList<GameInfo> currentList = new ArrayList<GameInfo>();
 		final ArrayList<GameInfo> openList = new ArrayList<GameInfo>();
-
+		boolean onLine = false;
+		final long date = Calendar.getInstance().getTimeInMillis();
 		for (final GameInfo info : fullList) {
 			if (info.getPlayers().contains(user)) {
-				currentList.add(info);
+
+				for (int i = 0; i < info.getGameSessions().size()
+						&& onLine == false; i++) {
+					if ((date - info.getGameSessions().get(i).getTimeInMillis() < 7200000)
+							&& (date
+									- info.getGameSessions().get(i).getTimeInMillis() > 0)) {
+						onLine = true;
+						currentList.add(info);
+					}
+				}
+				//currentList.add(info);
 			} else if (info.getnFreeTerritories() > 0) {
-				openList.add(info);
+				for (int i = 0; i < info.getGameSessions().size()
+						&& onLine == false; i++) {
+					if ((info.getGameSessions().get(i).after(Calendar.getInstance()))
+							|| (info.getGameSessions().get(i).DAY_OF_MONTH == Calendar.getInstance().DAY_OF_MONTH)) {
+						onLine = true;
+						openList.add(info);
+					}
+				}
+				//openList.add(info);
 			}
+			onLine = false;
 		}
 
 		mCurrentGameListModel.setData(currentList);
