9254e98f50f843982bf83216158418393cd8bc3b
Antonio Gomez <pobleteag@gmail.com>
2011-02-23 20:42:30 +0100

    Covertura de un 99 en mapModel

--- src/com/umbrella/worldconq/testing/MapModelTest.java
+++ src/com/umbrella/worldconq/testing/MapModelTest.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import junit.framework.TestCase;
 
 import com.umbrella.worldconq.domain.MapModel;
+import com.umbrella.worldconq.domain.PlayerListModel;
 import com.umbrella.worldconq.domain.TerritoryDecorator;
 
 import domain.Player;
+import domain.Spy;
 import domain.Territory;
 import domain.Territory.Continent;
 
@@ -15,6 +17,7 @@ public class MapModelTest extends TestCase {
 
 	private MapModel map;
 	private Player selfPlayer;
+	private PlayerListModel playerList;
 
 	private static final String[] colTitles = {
 			"Territorio", "Jugador", "Nº Soldados", "Nº Canones 1",
@@ -24,8 +27,15 @@ public class MapModelTest extends TestCase {
 
 	@Override
 	public void setUp() {
-		selfPlayer = new Player("Antonio", 100000);
+		selfPlayer = new Player("Antonio", 10000, true, true,
+			new ArrayList<Spy>());
 		map = new MapModel(selfPlayer);
+		final ArrayList<Player> players = new ArrayList<Player>();
+		players.add(selfPlayer);
+		players.add(new Player("Ambrosio", 1000, true, true,
+			new ArrayList<Spy>()));
+		playerList = new PlayerListModel(selfPlayer, players);
+
 	}
 
 	public void testMapModel() {
@@ -96,6 +106,47 @@ public class MapModelTest extends TestCase {
 
 	public void testgetValueAt5() {
 		System.out.println("TestCase:: testgetValueAt5");
+
+		final TerritoryDecorator T1 = new TerritoryDecorator(new Territory(0,
+			Continent.Africa, "Antonio", 10, new int[] {
+					0, 0, 0
+			}, 0, 0, 0), null, playerList);
+
+		map.updateTerritory(T1);
+
+		final TerritoryDecorator T2 = new TerritoryDecorator(new Territory(14,
+			Continent.Africa, "Ambrosio", 8, new int[] {
+					4, 9, 2
+			}, 3, 9, 0), null, playerList);
+
+		map.updateTerritory(T2);
+		final TerritoryDecorator T3 = new TerritoryDecorator(new Territory(6,
+			Continent.Africa, "", 9, new int[] {
+					0, 0, 0
+			}, 0, 0, 0), null, playerList);
+
+		map.updateTerritory(T3);
+
+		assertTrue(map.getValueAt(14, 1).equals("¿?"));
+		assertTrue(map.getValueAt(14, 0).equals(14));
+		assertTrue(map.getValueAt(6, 0).equals(6));
+		assertTrue(map.getValueAt(6, 2).equals("¿?"));
+
+		/* Se depliega un espia */
+
+		final ArrayList<Spy> spys = new ArrayList<Spy>();
+		spys.add(new Spy(14, 1));
+		selfPlayer.setSpies(spys);
+		playerList.updatePlayer(selfPlayer);
+		assertTrue(map.getValueAt(14, 0).equals(14));
+		assertTrue(map.getValueAt(14, 1).equals("Ambrosio"));
+		assertTrue(map.getValueAt(14, 2).equals(8));
+		assertTrue(map.getValueAt(14, 3).equals(4));
+		assertTrue(map.getValueAt(14, 4).equals(9));
+		assertTrue(map.getValueAt(14, 5).equals(2));
+		assertTrue(map.getValueAt(14, 6).equals(3));
+		assertTrue(map.getValueAt(14, 7).equals(9));
+		assertTrue(map.getValueAt(14, 8).equals(0));
 	}
 
 	@Override
