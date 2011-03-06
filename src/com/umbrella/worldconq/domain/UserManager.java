baa4e9310d3bd02279f1e343b16f07563011f2cd
Ricardo Ruedas <ricardo.ruedas@gmail.com>
2011-03-06 01:08:26 +0100

    Excepciones de UserManager.

--- src/com/umbrella/worldconq/domain/UserManager.java
+++ src/com/umbrella/worldconq/domain/UserManager.java
@@ -1,12 +1,21 @@
 package com.umbrella.worldconq.domain;
 
+import java.rmi.RemoteException;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.umbrella.worldconq.comm.ClientAdapter;
 import com.umbrella.worldconq.comm.ServerAdapter;
-import com.umbrella.worldconq.exceptions.InvalidArgumentException;
+import com.umbrella.worldconq.exceptions.EmptyStringException;
+import com.umbrella.worldconq.exceptions.MalformedEmailException;
+
+import exceptions.GameNotFoundException;
+import exceptions.InvalidSessionException;
+import exceptions.InvalidTimeException;
+import exceptions.NotCurrentPlayerGameException;
+import exceptions.UserAlreadyExistsException;
+import exceptions.WrongLoginException;
 
 public class UserManager {
 
@@ -42,35 +51,43 @@ public class UserManager {
 		return mSession;
 	}
 
-	public void registerUser(String login, String passwd, String email) throws Exception {
-		if (login == null || passwd == null || email == null ||
-				login.isEmpty() || passwd.isEmpty() || email.isEmpty()) {
-			throw new InvalidArgumentException();
-		} else {
-			final Pattern p = Pattern.compile(emailReEx);
-			final Matcher m = p.matcher(email);
-			if (m.find())
-				srvAdapter.registerUser(login, passwd, email);
-			else
-				throw new InvalidArgumentException();
-		}
+	public void registerUser(String login, String passwd, String email) throws RemoteException, UserAlreadyExistsException, MalformedEmailException {
+		if (login == null)
+			throw new NullPointerException();
+		if (login.isEmpty())
+			throw new EmptyStringException();
+		if (passwd == null)
+			throw new NullPointerException();
+		if (passwd.isEmpty())
+			throw new EmptyStringException();
+		if (email == null)
+			throw new NullPointerException();
+		if (email.isEmpty())
+			throw new EmptyStringException();
+
+		final Pattern p = Pattern.compile(emailReEx);
+		final Matcher m = p.matcher(email);
+		if (!m.find())
+			throw new MalformedEmailException();
+
+		srvAdapter.registerUser(login, passwd, email);
 	}
 
-	public void createSession(String login, String passwd) throws Exception {
+	public void createSession(String login, String passwd) throws RemoteException, GameNotFoundException, InvalidSessionException, InvalidTimeException, NotCurrentPlayerGameException, WrongLoginException {
 		if (login == null)
-			throw new InvalidArgumentException();
+			throw new NullPointerException();
 		if (passwd == null)
-			throw new InvalidArgumentException();
+			throw new NullPointerException();
 		if (login.isEmpty())
-			throw new InvalidArgumentException();
+			throw new EmptyStringException();
 		if (passwd.isEmpty())
-			throw new InvalidArgumentException();
+			throw new EmptyStringException();
 		if (mSession != null) this.closeSession();
 		final UUID id = srvAdapter.createSession(login, passwd, cltAdapter);
 		mSession = new Session(id, login);
 	}
 
-	public void closeSession() throws Exception {
+	public void closeSession() throws RemoteException, GameNotFoundException, InvalidSessionException, InvalidTimeException, NotCurrentPlayerGameException {
 		if (gameMgr.getGameEngine() != null) gameMgr.disconnectFromGame();
 		srvAdapter.closeSession(mSession);
 		mSession = null;
