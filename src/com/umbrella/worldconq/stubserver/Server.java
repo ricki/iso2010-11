b35d475bd3dc0cfb0cef22690d0d7de7a8ec6741
Ricardo Ruedas <ricardo.ruedas@gmail.com>
2011-03-07 20:09:51 +0100

    Casos de prueba de BuyUnits, DeploySpy y parte de MoveUnits
    Conflicts:
    
    	src/com/umbrella/worldconq/testing/GameEngineTest.java

--- src/com/umbrella/worldconq/stubserver/Server.java
+++ src/com/umbrella/worldconq/stubserver/Server.java
@@ -127,6 +127,10 @@ public class Server extends UnicastRemoteObject implements IServer {
 
 			mapList.set(0, new Territory(0, Territory.Continent.Europe,
 				playerList.get(1).getName(), 20, p2, 1, 6, 1));//terrotorio jorge
+			mapList.set(1, new Territory(1, Territory.Continent.Europe,
+				playerList.get(1).getName(), 20, p2, 1, 6, 1)); //territorio jorge adyacente al suyo
+			mapList.set(7, new Territory(7, Territory.Continent.Asia,
+				playerList.get(1).getName(), 20, p2, 1, 6, 1)); //territorio jorge no adyacente al suyo
 			mapList.set(2, t);
 
 			testGame.setMap(mapList);
